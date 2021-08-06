2af160618d40f5240b1e5b92bf0a4294283c43ad
fadhilr <fadilrizqullah97@gmail.com>
2021-08-06 21:15:04 +0700

    Perbaiki function tambah judul soal A

--- Post Test/soalA.js
+++ Post Test/soalA.js
@@ -1,26 +1,32 @@
-var rakBuku = [1,2,3,4,5];
-var judulBuku = prompt('Tambahkan buku ke rak dengan judul = ');
-
-
+var rakBuku = new Array(5);
+var judulBuku = prompt('Masukkan judul buku awalan = ');
 tambahBuku(judulBuku)
+cetak()
 
 var pinjam = parseInt(prompt('Pilih rak buku yang ingin dipinjam = ')) ;
 pinjamBuku(pinjam)
+cetak()
 
 var bukuKembali = prompt('Kembalikan buku ke rak dengan judul = ')
 var rakKembali = parseInt(prompt('Pilih rak buku yang ingin dikembalikan = ')) ;
 kembalikanBuku(rakKembali,bukuKembali)
+cetak()
 
-document.write(rakBuku.join('<br>'))
 function tambahBuku(judul){
     for (let index = 0; index < rakBuku.length; index++) {
-        rakBuku[index] = judul;
+        rakBuku[index] = judul+' '+(index+1);
     }
 }
 function pinjamBuku(indexRak){
-    rakBuku[indexRak] = undefined;
+    rakBuku[indexRak-1] = undefined;
 }
 
 function kembalikanBuku(indexRak, judul){
-    rakBuku[indexRak] = judul;
+    rakBuku[indexRak-1] = judul;
+}
+
+function cetak(){
+    document.write(rakBuku.join('<br>'))
+    document.write('<br>')
+    document.write('<br>')
 }
